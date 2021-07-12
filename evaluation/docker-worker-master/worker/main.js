9867593145a69d842362e4283fec545c209d993a
dherzog <david.herzog@helvetius.fr>
2021-07-12 14:31:51 +0200

    exo1

--- evaluation/docker-worker-master/worker/main.js
+++ evaluation/docker-worker-master/worker/main.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
 let mult = false
 let add = false
